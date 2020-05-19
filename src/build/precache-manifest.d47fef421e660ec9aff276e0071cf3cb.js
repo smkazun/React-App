98bfa1c636c0cc2853503f10075c6836a5780505
Sebastian K <smkazun@iastate.edu>
2020-05-19 17:43:13 -0500

    fixing for deployment

---
+++ src/build/precache-manifest.d47fef421e660ec9aff276e0071cf3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e40f820b32b1cdf738f17737757f2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1e0d9a2e3f9d7081579",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3786a7c7cc89fc999afc",
+    "url": "/static/js/2.50db73b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50db73b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e0d9a2e3f9d7081579",
+    "url": "/static/js/main.6c4f1bb0.chunk.js"
+  },
+  {
+    "revision": "a07415f3a612dc5038b4",
+    "url": "/static/js/runtime-main.2636bc25.js"
+  }
+]);
